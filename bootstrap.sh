f28576ee24da09fb195287bdb38e169b556adea7
Vincent Shen <wenshen@redhat.com>
2025-06-09 22:11:51 -0700

    Address enterprise contract failure

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -x
-
+OCP_VERSIONS=4.18
 for VERSION in ${OCP_VERSIONS}
 do
     CONTAINERFILE="Containerfile-rhel-9.in"
@@ -8,12 +8,18 @@ do
             CONTAINERFILE="Containerfile-rhel-8.in"
     fi
 
+
     opm migrate "registry.redhat.io/redhat/redhat-operator-index:v${VERSION}" "./catalog-migrate-${VERSION}"
     mkdir -p "catalog/v${VERSION}/file-integrity-operator"
     cp "${CONTAINERFILE}" "catalog/v${VERSION}/Containerfile"
     sed -i "s/OCP_VERSION/${VERSION}/g" "catalog/v${VERSION}/Containerfile"
     opm alpha convert-template basic -o yaml "./catalog-migrate-${VERSION}/file-integrity-operator/catalog.json" > "catalog/v${VERSION}/catalog-template.yaml"
-    opm alpha render-template basic -o yaml "catalog/v${VERSION}/catalog-template.yaml" > "catalog/v${VERSION}/file-integrity-operator/catalog.yaml"
+    # # --- 1) Render the new bundle into a temp file ---
+    if [[ "$VERSION" =~ ("4.12"|"4.13"|"4.14"|"4.15"|"4.16") ]]; then
+        opm alpha render-template basic -o yaml "catalog/v${VERSION}/catalog-template.yaml" > "catalog/v${VERSION}/file-integrity-operator/catalog.yaml"
+    else
+        opm alpha render-template basic -o yaml "catalog/v${VERSION}/catalog-template.yaml" --migrate-level=bundle-object-to-csv-metadata > "catalog/v${VERSION}/file-integrity-operator/catalog.yaml"
+    fi
 
     echo "Building locally to ensure it works"
     podman build -t "fio-fbc-${VERSION}" -f "catalog/v${VERSION}/Containerfile" "catalog/v${VERSION}/" && rm -rf "./catalog-migrate-${VERSION}"
